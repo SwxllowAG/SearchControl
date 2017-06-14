4bc5b363e5f134eaaa5976e8f085bdb970d1c031
Zhanserik Kenes <zhans.kenes@gmail.com>
2017-06-14 12:17:13 +0600

    Initial Commit

---
+++ SearchControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchControl
+//
+//  Created by Zhanserik on 6/14/17.
+//  Copyright © 2017 Kenes Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
